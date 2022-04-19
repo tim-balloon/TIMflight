8b85830a656d621c860cd8b62af2f58ab756b6f1
Brock <brendal4@illinois.edu>
2022-04-19 13:31:28 -0500

    Add emulator repo with UDP writer/listener scripts

---
+++ emulator/UDPlistener.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define GETSOCKETERRNO() (errno)
+
+int main()
+{
+    //char message[2000];   //Use these 2 lines for sending strings
+    //char reply[2000];
+
+    float message;
+    float reply;
+
+    //Create socket:
+    printf("Creating socket...\n");
+    int my_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    printf("Status: %s\n", strerror(errno));
+
+    //Initialize server address (loopback)
+    printf("Initializing server address...\n");
+    struct sockaddr_in listener_address, client_address;
+    listener_address.sin_family = AF_INET;
+    listener_address.sin_port = htons(2000);
+    listener_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    int flags=0;
+    int listener_address_size = sizeof(listener_address);
+    int client_address_size = sizeof(client_address);
+
+    //Bind to port and IP
+    printf("Binding to port and IP...\n");    //ADD BETTER ERROR HANDLING HERE
+    bind(my_socket, (struct sockaddr *) &listener_address, listener_address_size);
+    printf("Status: %s\n", strerror(errno));
+    printf("Listening...\n");  //Maybe add what port/IP it's listening on?
+
+    //Revieve any incoming messages
+    // If sending/recieving strings, use strlen(message) instead of sizeof(message)
+    int recieving_func = recvfrom(my_socket, &message, sizeof(message), flags, 
+                          (struct sockaddr*)&client_address, &client_address_size);
+
+    if (recieving_func < 0) {
+        printf("Failed to recieve message\n");
+        return -1;
+    }
+    else if (recieving_func == 0) {
+        printf("Connection closed on the other side!");
+    }
+    
+    printf("Client says: %f\n", message);
+
+    //strcpy(reply, message);   //Use this line for strings
+    reply = message;
+
+    //Send reply
+    if (sendto(my_socket, &reply, sizeof(reply), flags, (struct sockaddr*)&client_address,
+                client_address_size) < 0) {
+        printf("Failed to send reply\n");
+        printf("The last error message is: %s\n", strerror(errno));
+        return -1;
+        }
+
+    //Close socket
+    close(my_socket);
+}
---
+++ emulator/UDPwriter.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define GETSOCKETERRNO() (errno)
+
+int main()
+{
+    // char message[2000];         //Use these two lines for sending/recieving strings
+    // char server_message[2000];
+    float server_message;
+    float message;
+    int message_size = sizeof(message);
+    int reply_size = sizeof(server_message);
+    unsigned int flags=0;
+
+    //Create socket
+    printf("Creating socket...\n");
+    int my_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    printf("Status: %s\n", strerror(errno));
+
+    //Initialize server address
+    printf("Initializing server address...\n");
+    struct sockaddr_in client_address;   //ina = internet address
+    client_address.sin_family = AF_INET;  //Use IPv4
+    client_address.sin_port = htons(2000);  //Port number (it's gotta be in network order, hence htons()!)
+    client_address.sin_addr.s_addr = inet_addr("127.0.0.1");  //Sets address
+    if (client_address.sin_addr.s_addr < 0) {
+        printf("Error setting address!");
+    }
+    else if (client_address.sin_addr.s_addr == 0) {
+        printf("Address is messed up!");
+    }
+    int sockaddr_size = sizeof(client_address);
+    memset(&client_address.sin_zero, 0, sizeof(client_address.sin_zero));
+    printf("Status: %s\n", strerror(errno));
+
+    //Write message
+    // printf("Enter a phrase to be sent: ");    //Use these two lines for sending strings
+    // fgets(message, sizeof(message), stdin);
+    printf("Enter a float to be sent: ");
+    scanf("%f", &message);
+    
+    //Send message
+    if (sendto(my_socket, &message, sizeof(message), flags, (struct sockaddr*)&client_address,
+        sockaddr_size) < 0) {
+        printf("Failed to send\n");
+        printf("The last error message is: %s\n", strerror(errno));
+        return -1;
+    }
+
+    //Receive reply from server
+    int recieving_func = recvfrom(my_socket, &server_message, reply_size, flags, 
+                            (struct sockaddr*)&client_address, &sockaddr_size);
+
+    if (recieving_func < 0) {
+        printf("Error when receiving reply\n");
+        printf("The last error message is: %s\n", strerror(errno));
+        return -1;
+    }
+    else if (recieving_func == 0) {
+        printf("Connection closed on the other side!");
+        return -1;
+    }
+
+    //What's the response from the server?
+    printf("Server response is: %f", server_message);
+
+    //Close socket
+    close(my_socket);
+
+    return 0;
+}

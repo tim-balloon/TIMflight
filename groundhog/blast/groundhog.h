07e7136c3b4ab72f61c2b901fedb408db6b6022c
Shubh Agrawal <shubhagrawal30@gmail.com>
2023-09-25 16:01:46 -0700

    basic structure for EVTM groundhog

--- groundhog/blast/groundhog.h
+++ groundhog/blast/groundhog.h
@@ -18,7 +18,7 @@
 #ifndef MAX
 #define MAX(a, b) ((a) > (b) ? (a):(b))
 #endif
-enum DownLinkTypes {PILOT, BI0, HIGHRATE, 
+enum DownLinkTypes {PILOT, BI0, HIGHRATE, TDRSS_EVTM, LOS_EVTM,
                       NUM_DOWNLINKS};
 
 // BLAST pilot
@@ -37,3 +37,26 @@ void biphase_receive(void *arg);
 #include "comms_serial.h"
 void highrate_receive(void *arg);
 
+// TIM EVTM
+struct EVTMRecvSetup {
+    struct TlmReport *report;
+    struct UDPSetup *udpsetup;
+    uint8_t *recvbuffer;
+    uint32_t serial;
+    uint32_t prev_serial;
+    linklist_t *ll;
+    int32_t blk_size;
+    uint32_t recv_size;
+    uint32_t transmit_size;
+    int64_t framenum;
+    int af;
+    uint8_t *local_allframe;
+    linklist_rawfile_t *ll_rawfile;
+    uint8_t *compbuffer;
+    int bad_serial_count;
+};
+#include "evtm.h"
+void EVTM_setup_receiver(void *arg, struct EVTMRecvSetup *es);
+void EVTM_receiver_get_linklist(struct EVTMRecvSetup *es);
+void EVTM_receiver_loop_body(struct EVTMRecvSetup *es);
+void EVTM_udp_receive(void *arg);
--- groundhog/blast/main.c
+++ groundhog/blast/main.c
@@ -47,15 +47,22 @@ int main(int argc, char * argv[]) {
   int pilot_on = 1;
   int bi0_on = 1;
   int highrate_on = 1;
+  int evtm_los_on = 1;
+  int evtm_tdrss_on = 1;
   int daemon = 0;
 
   for (int i = 1; i < argc; i++) {
     if (strcmp(argv[i], "-no_pilot") == 0) pilot_on = 0;
     else if (strcmp(argv[i], "-no_bi0") == 0) bi0_on = 0;
     else if (strcmp(argv[i], "-no_highrate") == 0) highrate_on = 0;
-    else if (strcmp(argv[i], "-pilot_only") == 0) bi0_on = highrate_on = 0;
-    else if (strcmp(argv[i], "-bi0_only") == 0) highrate_on = pilot_on = 0;
-    else if (strcmp(argv[i], "-highrate_only") == 0) pilot_on = bi0_on = 0;
+    else if (strcmp(argv[i], "-no_evtm_tdrss") == 0) evtm_tdrss_on = 0;
+    else if (strcmp(argv[i], "-no_evtm_los") == 0) evtm_los_on = 0;
+    else if (strcmp(argv[i], "-pilot_only") == 0) bi0_on = highrate_on = evtm_tdrss_on = evtm_los_on = 0;
+    else if (strcmp(argv[i], "-bi0_only") == 0) highrate_on = pilot_on = evtm_tdrss_on = evtm_los_on = 0;
+    else if (strcmp(argv[i], "-highrate_only") == 0) pilot_on = bi0_on = evtm_tdrss_on = evtm_los_on = 0;
+    else if (strcmp(argv[i], "-evtm_los_only") == 0) pilot_on = bi0_on = highrate_on = evtm_tdrss_on = 0;
+    else if (strcmp(argv[i], "-evtm_tdrss_only") == 0) pilot_on = bi0_on = highrate_on = evtm_los_on = 0;
+    else if (strcmp(argv[i], "-evtm_only") == 0) pilot_on = bi0_on = highrate_on = 0;
     else if (strcmp(argv[i], "-d") == 0) daemon = 1;
     else if (strcmp(argv[i], "-quiet") == 0) verbose = 0;
     else if (strcmp(argv[i], "-verbose") == 0) verbose = 1;
@@ -84,11 +91,27 @@ int main(int argc, char * argv[]) {
                                  PILOT_MAX_PACKET_SIZE,
                                  PILOT};
 
+  struct UDPSetup evtm_los_setup = {"EVTM LOS", 
+                                    EVTM_ADDR_LOS, 
+                                    EVTM_PORT_LOS, 
+                                    EVTM_MAX_SIZE, 
+                                    EVTM_MAX_PACKET_SIZE,
+                                    LOS_EVTM};
+
+  struct UDPSetup evtm_tdrss_setup = {"EVTM TDRSS", 
+                                      EVTM_ADDR_TDRSS, 
+                                      EVTM_PORT_TDRSS, 
+                                      EVTM_MAX_SIZE, 
+                                      EVTM_MAX_PACKET_SIZE,
+                                      TDRSS_EVTM};
+                                    
   // Receiving data from telemetry
   pthread_t pilot_receive_worker[2];
   pthread_t biphase_receive_worker;
   pthread_t highrate_receive_worker;
   pthread_t direct_receive_worker;
+  pthread_t evtm_los_receive_worker;
+  pthread_t evtm_tdrss_receive_worker;
 
   // Serving up data received via telemetry
   pthread_t server_thread;
@@ -105,6 +128,14 @@ int main(int argc, char * argv[]) {
   if (highrate_on) {
     pthread_create(&highrate_receive_worker, NULL, (void *) &highrate_receive, (void *) 0);
     pthread_create(&direct_receive_worker, NULL, (void *) &highrate_receive, (void *) 1);
+  }
+
+  if (evtm_los_on) {
+    pthread_create(&evtm_los_receive_worker, NULL, (void *) &EVTM_udp_receive, (void *) &evtm_los_setup);
+  }
+
+  if (evtm_tdrss_on) {
+    pthread_create(&evtm_tdrss_receive_worker, NULL, (void *) &EVTM_udp_receive, (void *) &evtm_tdrss_setup);
   }
 
   // start the server thread for mole clients
